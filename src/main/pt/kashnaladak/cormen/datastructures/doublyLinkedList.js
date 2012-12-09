234ffca86d14e58e1e78d3f7950c83a690929590
Kashnaladak <francisco.paulo@gmail.com>
2012-12-09 12:16:06 +0100

    Added .gitignore for .idea folder

--- src/main/pt/kashnaladak/cormen/datastructures/doublyLinkedList.js
+++ src/main/pt/kashnaladak/cormen/datastructures/doublyLinkedList.js
@@ -2,11 +2,42 @@
 Kash.namespace('Kash.cormen.datastructures');
 
 Kash.cormen.datastructures.DoublyLinkedList = function(){
-    var head = null;
+    var _head = null;
+
+    function Element(elem, next, prev){
+        var _elem = elem;
+        var _next = next;
+        var _prev = prev;
+
+        return {
+            getElem: function(){
+                return elem;
+            },
+            setNext: function(next){
+                _next = next;
+            },
+            getNext: function(){
+                return _next;
+            },
+            setPrev: function(){
+                _prev = prev;
+            },
+            getPrev: function(){
+                return _prev;
+            }
+        }
+    }
 
     return {
         isEmpty: function(){
             return _.isNull(head);
+        },
+        search: function(elem){
+            var x = _head;
+            while(!_.isNull(x) && !_.isEqual(x.getElem(), elem)){
+                x = x.getNext();
+            }
+            return x;
         }
     }
 }
