eb7b87e09d1db2758a3151c27f5981ff0588333f
Kashnaladak <francisco.paulo@gmail.com>
2012-12-09 13:30:14 +0100

    Changes function definitions to operate on the prototype rather than on the object instance itself

--- src/main/pt/kashnaladak/cormen/datastructures/queue.js
+++ src/main/pt/kashnaladak/cormen/datastructures/queue.js
@@ -2,24 +2,31 @@
 Kash.namespace('Kash.cormen.datastructures');
 
 Kash.cormen.datastructures.Queue = function(){
+    var clazz = Kash.cormen.datastructures.Queue;
     var innerArray = [];
 
-    return {
-        enqueue: function(elem) {
-            if(_.isUndefined(elem)){
-                throw new Error("Unable to enqueue undefined element.");
-            }
-            innerArray.push(elem);
-        },
-        dequeue: function(){
-            if(this.isEmpty()){
-                throw new Error("Unable to dequeue an emtpy queue.");
-            }
-            return innerArray.shift();
-        },
-        isEmpty: function(){
-            return innerArray.length == 0
+    clazz.prototype._enqueue = function(elem) {
+        if(_.isUndefined(elem)){
+            throw new Error("Unable to enqueue undefined element.");
         }
+        innerArray.push(elem);
+    }
+
+    clazz.prototype._dequeue = function(){
+        if(this.isEmpty()){
+            throw new Error("Unable to dequeue an emtpy queue.");
+        }
+        return innerArray.shift();
+    }
+
+    clazz.prototype._isEmpty = function(){
+        return innerArray.length == 0
+    }
+
+    return {
+        enqueue: clazz.prototype._enqueue,
+        dequeue: clazz.prototype._dequeue,
+        isEmpty: clazz.prototype._isEmpty
     }
 }
 
--- src/main/pt/kashnaladak/cormen/datastructures/stack.js
+++ src/main/pt/kashnaladak/cormen/datastructures/stack.js
@@ -2,24 +2,31 @@
 Kash.namespace('Kash.cormen.datastructures');
 
 Kash.cormen.datastructures.Stack = function(){
+    var clazz = Kash.cormen.datastructures.Stack;
     var innerArray = [];
 
-    return {
-        push: function(elem){
-            if(_.isUndefined(elem)){
-                throw new Error("Unable to push undefined element.");
-            }
-            innerArray.push(elem);
-        },
-        pop: function(){
-            if(this.isEmpty()){
-                throw new Error("Unable to pop an emtpy stack.");
-            }
-            return innerArray.pop();
-        },
-        isEmpty: function(){
-            return innerArray.length == 0
+    clazz.prototype._push = function(elem){
+        if(_.isUndefined(elem)){
+            throw new Error("Unable to push undefined element.");
         }
+        innerArray.push(elem);
+    }
+
+    clazz.prototype._pop = function(){
+        if(this.isEmpty()){
+            throw new Error("Unable to pop an emtpy stack.");
+        }
+        return innerArray.pop();
+    }
+
+    clazz.prototype._isEmpty = function(){
+        return innerArray.length == 0
+    }
+
+    return {
+        push: clazz.prototype._push,
+        pop: clazz.prototype._pop,
+        isEmpty: clazz.prototype._isEmpty
     }
 }
 
