3649e5fc78e5116eb26eed6184884ca639bd3436
Kashnaladak <francisco.paulo@gmail.com>
2012-12-16 18:31:34 +0100

    Updates on the doubly linked list implementation.

--- src/main/pt/kashnaladak/cormen/datastructures/doublyLinkedList.js
+++ src/main/pt/kashnaladak/cormen/datastructures/doublyLinkedList.js
@@ -3,42 +3,62 @@ Kash.namespace('Kash.cormen.datastructures');
 
 Kash.cormen.datastructures.DoublyLinkedList = function(){
     var _head = null;
+    var _tail = null;
 
-    function Element(elem, next, prev){
-        var _elem = elem;
-        var _next = next;
-        var _prev = prev;
-
+    function Element(value, next, prev){
         return {
-            getElem: function(){
-                return elem;
-            },
-            setNext: function(next){
-                _next = next;
-            },
-            getNext: function(){
-                return _next;
-            },
-            setPrev: function(){
-                _prev = prev;
-            },
-            getPrev: function(){
-                return _prev;
+            'value': value,
+            'next': next,
+            'prev': prev
+        }
+    }
+
+    function _isEmpty(){
+        return _.isNull(_head);
+    }
+
+    function _search(value){
+        for(var elem = _head; !_.isNull(elem) && !_.isEqual(elem.value, value); elem = elem.next){}
+        return elem;
+    }
+
+    function _insert(value){
+        var elem = new Element(value, null, _tail);
+        _tail = elem;
+        if(!_.isNull(elem.prev)){
+            elem.prev.next = elem
+        }
+        if(_.isNull(_head)){
+            _head = elem;
+        }
+    }
+
+    function _remove(value){
+        var elem = _search(value);
+        if(!_.isNull(elem)){
+            if(_.isEqual(elem, _head)){
+                _head = elem.next;
             }
+            else {
+                elem.prev.next = elem.next;
+            }
+            if(_.isEqual(elem, _tail)){
+                _tail = elem.prev;
+            }
+            else {
+                elem.next.prev = elem.prev;
+            }
+        }
+        else {
+            throw "No element found for value: " + value;
         }
     }
 
     return {
-        isEmpty: function(){
-            return _.isNull(head);
-        },
-        search: function(elem){
-            var x = _head;
-            while(!_.isNull(x) && !_.isEqual(x.getElem(), elem)){
-                x = x.getNext();
-            }
-            return x;
-        }
+        isEmpty: _isEmpty,
+        search: _search,
+        insert: _insert,
+        remove: _remove
     }
-}
+};
 
--- src/test/pt/kashnaladak/cormen/datastructures/doublyLinkedListTest.js
+++ src/test/pt/kashnaladak/cormen/datastructures/doublyLinkedListTest.js
@@ -1,11 +1,60 @@
 DoublyLinkedListTest = TestCase("DoublyLinkedListTest");
 
-DoublyLinkedListTest.prototype.testSimpleDoublyLinkedList = function() {
+
+
+DoublyLinkedListTest.prototype.testCreateDoublyLinkedList = function() {
     var dll = new Kash.cormen.datastructures.DoublyLinkedList();
     assertObject(dll);
+    assertFunction(dll.isEmpty);
+    assertFunction(dll.insert);
+    assertFunction(dll.remove);
 };
 
 DoublyLinkedListTest.prototype.testIsEmptyDoublyLinkedList = function() {
     var dll = new Kash.cormen.datastructures.DoublyLinkedList();
     assertTrue(dll.isEmpty());
+};
+
+DoublyLinkedListTest.prototype.testSearchDoublyLinkedList = function() {
+    var dll = new Kash.cormen.datastructures.DoublyLinkedList();
+    assertNull(dll.search('this is not on the list'));
+};
+
+DoublyLinkedListTest.prototype.testInsertDoublyLinkedList = function() {
+    var dll = new Kash.cormen.datastructures.DoublyLinkedList();
+    dll.insert(1);
+    assertFalse(dll.isEmpty());
+
+    var elem = dll.search(1);
+    assertNull(elem.next);
+    assertNull(elem.prev);
+    assertEquals(elem.value, 1);
+
+    dll.insert(2);
+    var elem2 = dll.search(2);
+    assertNull(elem2.next);
+    assertNotNull(elem2.prev);
+    assertEquals(elem2.prev, elem);
+    assertEquals(elem.next, elem2);
+};
+
+DoublyLinkedListTest.prototype.testRemoveDoublyLinkedList = function() {
+    var dll = new Kash.cormen.datastructures.DoublyLinkedList();
+
+    assertException(function(){
+        dll.remove(1);
+    });
+
+    dll.insert(1);
+    dll.remove(1);
+    assertTrue(dll.isEmpty());
+
+    dll.insert(1);
+    dll.insert(2);
+    dll.remove(1);
+    assertNull(dll.search(1));
+
+    var elem = dll.search(2);
+    assertNull(elem.prev);
+    assertNull(elem.next);
 };
